eb9b3c0d7fbf4ef8b8ab4784e2473bdc4c2f5fc0
Rishi Kumar Singh <rishisingh43210@gmail.com>
2024-06-08 22:49:24 +0530

    quote changed

--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -118,7 +118,7 @@ const Chat = () => {
           <img src={user?.avatar || "./avatar.png"} alt="" />
           <div className="texts">
             <span>{user?.username}</span>
-            <p>Lorem ipsum dolor, sit amet.</p>
+            <p>last seen today</p>
           </div>
         </div>
         <div className="icons">
